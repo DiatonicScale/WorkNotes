e965a14519f59849abfcb40c5cff9c45b7ad67b5
euglav127 <euglav127@gmail.com>
2018-09-04 19:56:41 +0300

    - Added populating of creation_time and last_edit_time in populateDB.sql script

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -4,29 +4,29 @@ DELETE FROM categories;
 DELETE FROM notes;
 ALTER SEQUENCE global_seq RESTART WITH 10000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User0', 'user0@gmail.com', 'password0'),
-  ('User1', 'user1@yandex.ru', 'password1');
+INSERT INTO users (name, email, password, registered) VALUES
+  ('User0', 'user0@gmail.com', 'password0', '2018-08-20 10:00:00'),
+  ('User1', 'user1@yandex.ru', 'password1', '2018-08-21 15:00:00');
 
 INSERT INTO users_roles (user_id, role) VALUES
   (10000, 'ROLE_USER'),
   (10001, 'ROLE_ADMIN');
 
-INSERT INTO categories (user_id, name) VALUES
-  (10000, 'Java'),
-  (10000, 'Git'),
-  (10000, 'Maven'),
-  (10001, 'Python'),
-  (10001, '.NET');
+INSERT INTO categories (user_id, name, creation_time, last_edit_time) VALUES
+  (10000, 'Java', '2018-08-30 10:00:00', '2018-08-30 10:00:00'),
+  (10000, 'Git', '2018-08-29 20:00:00', '2018-08-29 20:00:00'),
+  (10000, 'Maven', '2018-08-31 13:00:00', '2018-08-31 13:00:00'),
+  (10001, 'Python', '2018-08-25 14:00:00', '2018-08-25 14:00:00'),
+  (10001, '.NET', '2018-09-02 21:00:00', '2018-09-02 21:00:00');
 
-INSERT INTO notes (category_id, name, value) VALUES
-  (10002, 'Final keyword', 'value final'),
-  (10003, 'Clone command', 'value clone'),
-  (10003, 'Commit', 'value commit'),
-  (10003, 'Push', 'value push'),
-  (10002, 'Remote debug', 'value remote'),
-  (10004, 'Dependencies', 'value depend'),
-  (10002, 'Servlets', 'value servlet'),
-  (10004, 'Skip tests mode', 'value skip test'),
-  (10005, 'Python basics', 'value python basics'),
-  (10006, 'Interfaces in C#', 'value c# interfaces');
+INSERT INTO notes (category_id, name, value, creation_time, last_edit_time) VALUES
+  (10002, 'Final keyword', 'value final', '2018-08-30 11:30:00', '2018-08-30 11:30:00'),
+  (10003, 'Clone command', 'value clone', '2018-08-31 23:12:00', '2018-08-31 23:12:00'),
+  (10003, 'Commit', 'value commit', '2018-09-01 11:00:00', '2018-09-01 11:00:00'),
+  (10003, 'Push', 'value push', '2018-09-02 20:00:00', '2018-09-02 20:00:00'),
+  (10002, 'Remote debug', 'value remote', '2018-08-30 10:00:00', '2018-08-30 10:00:00'),
+  (10004, 'Dependencies', 'value depend', '2018-08-31 07:20:00', '2018-08-31 07:20:00'),
+  (10002, 'Servlets', 'value servlet', '2018-08-30 01:02:00', '2018-08-30 01:02:00'),
+  (10004, 'Skip tests mode', 'value skip test', '2018-08-31 17:00:00', '2018-08-31 17:00:00'),
+  (10005, 'Python basics', 'value python basics', '2018-08-25 09:00:00', '2018-08-25 09:00:00'),
+  (10006, 'Interfaces in C#', 'value c# interfaces', '2018-09-02 15:41:00', '2018-09-02 15:41:00');
